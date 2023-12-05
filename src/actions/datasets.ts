6cb368c98e4ba95ae98f6c844a237cd732da175d
GUO YANKE <hi@guoyk.xyz>
2023-12-05 16:13:30 +0800

    fix: fix cloudflare types bug

--- src/actions/datasets.ts
+++ src/actions/datasets.ts
@@ -8,13 +8,13 @@ import { chunk, groupBy } from 'lodash';
 const MODEL_EMBEDDINGS = '@cf/baai/bge-base-en-v1.5';
 
 type DocumentPayload = {
-	id: string
-	repo: string
-	url: string
-	title: string
-	content: string
-	sentences: string[]
-}
+	id: string;
+	repo: string;
+	url: string;
+	title: string;
+	content: string;
+	sentences: string[];
+};
 
 export const datasets_document_upsert: ActionHandler = async (
 	{ env },
@@ -23,12 +23,19 @@ export const datasets_document_upsert: ActionHandler = async (
 	const db = drizzle(env.DB_DATASETS);
 
 	// upsert document
-	await db.insert(tDocuments).values({
-		id, repo, url, title, content
-	}).onConflictDoUpdate({
-		target: tDocuments.id,
-		set: { repo, url, title, content }
-	});
+	await db
+		.insert(tDocuments)
+		.values({
+			id,
+			repo,
+			url,
+			title,
+			content,
+		})
+		.onConflictDoUpdate({
+			target: tDocuments.id,
+			set: { repo, url, title, content },
+		});
 
 	// delete old sentences
 	await db.delete(tSentences).where(eq(tSentences.documentId, id));
@@ -40,36 +47,35 @@ export const datasets_document_upsert: ActionHandler = async (
 		documentId: id,
 		position: position,
 		content: sentence,
-		status: 0
+		status: 0,
 	}));
 
 	// insert sentences
-	await Promise.all(
-		chunk(sentences, 10).map(sentences => db.insert(tSentences).values(sentences))
-	);
+	await Promise.all(chunk(sentences, 10).map((sentences) => db.insert(tSentences).values(sentences)));
 
 	// invoke vectorize action
 	await Promise.all(
-		chunk(sentences, 10).map(sentences => env.QUEUE_DATASETS_VECTORIZE_SENTENCES_UPSERT.sendBatch(
-			sentences.map(record => ({ body: { sentenceId: record.id }, contentType: 'json' }))
-		))
+		chunk(sentences, 10).map((sentences) =>
+			env.QUEUE_DATASETS_VECTORIZE_SENTENCES_UPSERT.sendBatch(
+				sentences.map((record) => ({ body: { sentenceId: record.id }, contentType: 'json' }))
+			)
+		)
 	);
 
 	return {
 		success: true,
 		document: {
-			id
+			id,
 		},
-		sentences: sentences.map(({ id }) => ({ id }))
+		sentences: sentences.map(({ id }) => ({ id })),
 	};
 };
-
 
 export const datasets_vectorize_sentences_upsert: ActionHandler = async ({ env }, { sentenceId }) => {
 	const db = drizzle(env.DB_DATASETS, { schema: { tDocuments, tSentences } });
 	// fetch sentence
 	const sentence = await db.query.tSentences.findFirst({
-		where: eq(tSentences.id, sentenceId)
+		where: eq(tSentences.id, sentenceId),
 	});
 	if (!sentence) {
 		throw new Error(`Sentence ${sentenceId} not found`);
@@ -86,23 +92,27 @@ export const datasets_vectorize_sentences_upsert: ActionHandler = async ({ env }
 		throw new Error(`Failed to vectorize sentence ${sentenceId}`);
 	}
 	// upsert sentence
-	await env.VECTORIZE_DATASETS_SENTENCES.upsert([{
-		id: sentenceId,
-		values,
-		metadata: {
-			repo: sentence.repo,
-			documentId: sentence.documentId,
-			position: sentence.position
-		}
-	}]);
+	await env.VECTORIZE_DATASETS_SENTENCES.upsert([
+		{
+			id: sentenceId,
+			values,
+			metadata: {
+				repo: sentence.repo,
+				documentId: sentence.documentId,
+				position: sentence.position,
+			},
+		},
+	]);
 	// update status
-	await db.update(tSentences).set({
-		status: 1
-	}).where(eq(tSentences.id, sentenceId));
+	await db
+		.update(tSentences)
+		.set({
+			status: 1,
+		})
+		.where(eq(tSentences.id, sentenceId));
 
 	return { success: true };
 };
-
 
 const RETRIEVE_SIMILARITY_CUTOFF = 0.75;
 const RETRIEVE_TOP_K = 2;
@@ -119,16 +129,17 @@ export const datasets_vectorize_sentences_retrieve: ActionHandler = async ({ env
 	const vectorQuery = await env.VECTORIZE_DATASETS_SENTENCES.query(values, { topK: RETRIEVE_TOP_K });
 	// build query range
 
+	// cloudflare types bug, vec id should be returned as vec.vectorId
 	const sentencesIds = vectorQuery.matches
-		.filter(vec => vec.score > RETRIEVE_SIMILARITY_CUTOFF)
-		.map(vec => vec.id);
+		.filter((vec) => vec.score > RETRIEVE_SIMILARITY_CUTOFF)
+		.map((vec: any) => vec.id || vec.vectorId);
 
 	if (!sentencesIds.length) {
 		return [];
 	}
 
 	const sentences = await db.query.tSentences.findMany({
-		where: inArray(tSentences.id, sentencesIds)
+		where: inArray(tSentences.id, sentencesIds),
 	});
 
 	const sentenceGroups = groupBy(sentences, 'documentId');
@@ -141,13 +152,14 @@ export const datasets_vectorize_sentences_retrieve: ActionHandler = async ({ env
 			where: and(
 				eq(tSentences.documentId, documentId),
 				or(
-					...
-						sentenceGroups[documentId].map(record => and(
+					...sentenceGroups[documentId].map((record) =>
+						and(
 							gte(tSentences.position, record.position - RETRIEVE_CONTEXT_SIZE),
 							lte(tSentences.position, record.position + RETRIEVE_CONTEXT_SIZE)
-						))
+						)
+					)
 				)
-			)
+			),
 		});
 		results.push({ document, sentences: sentences });
 	}
