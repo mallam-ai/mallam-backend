b1dd42e53fe54968e697c7892eaa2995ec3be1b7
GUO YANKE <hi@yankeguo.com>
2024-01-09 22:06:14 +0800

    fix: expose team_create function, #4

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,7 +2,7 @@ import { ActionHandler } from '../types';
 import { datasets_document_upsert, datasets_vectorize_sentences_retrieve, datasets_vectorize_sentences_upsert } from './datasets';
 import { oauth_authorize_user, oauth_create_authorization_uri } from './auth';
 import { user_get } from './users';
-
+import { team_create } from './teams';
 import { chat } from './chat';
 import { ping } from './ping';
 
@@ -15,6 +15,7 @@ const ACTIONS = {
 	oauth_create_authorization_uri,
 	oauth_authorize_user,
 	user_get,
+	team_create,
 	// queues
 	'queue-mallam-datasets-vectorize-sentences-upsert': datasets_vectorize_sentences_upsert,
 };
