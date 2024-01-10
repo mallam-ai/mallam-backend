29a26ff871d5ae1662267c60d98679250d658fb5
GUO YANKE <hi@yankeguo.com>
2024-01-10 15:19:53 +0800

    feat: add membership_remove, membership_add, #7

--- src/actions/memberships.ts
+++ src/actions/memberships.ts
@@ -4,6 +4,106 @@ import { drizzle } from 'drizzle-orm/d1';
 import { sql, and, isNull, eq, inArray } from 'drizzle-orm';
 import { halt } from '../utils';
 
+export const membership_add: ActionHandler = async function (
+	{ env },
+	{
+		userId,
+		teamId,
+		role,
+		targetUserId,
+	}: {
+		userId: string;
+		teamId: string;
+		role: string;
+		targetUserId: string;
+	}
+) {
+	if (targetUserId === userId) {
+		halt(`cannot add yourself`, 403);
+	}
+
+	const db = drizzle(env.DB_MAIN, { schema });
+
+	const team = await db.query.tTeams.findFirst({ where: and(eq(schema.tTeams.id, teamId), isNull(schema.tTeams.deletedAt)) });
+
+	if (!team) {
+		halt(`team ${teamId} not found`, 404);
+	}
+
+	{
+		const membership = await db.query.tMemberships.findFirst({
+			where: and(
+				eq(schema.tMemberships.userId, userId),
+				eq(schema.tMemberships.teamId, teamId),
+				eq(schema.tMemberships.role, schema.membershipRoles.admin)
+			),
+		});
+
+		if (!membership) {
+			halt(`user ${userId} is not an admin of team ${teamId}`, 403);
+		}
+	}
+
+	const membership = await db
+		.insert(schema.tMemberships)
+		.values({
+			id: crypto.randomUUID(),
+			userId: targetUserId,
+			teamId: teamId,
+			role: role,
+			createdAt: new Date(),
+			createdBy: userId,
+		})
+		.onConflictDoUpdate({
+			target: schema.tMemberships.id,
+			set: { role: role },
+		})
+		.returning();
+
+	return { membership };
+};
+
+export const membership_remove: ActionHandler = async function (
+	{ env },
+	{
+		userId,
+		teamId,
+		targetUserId,
+	}: {
+		userId: string;
+		teamId: string;
+		targetUserId: string;
+	}
+) {
+	if (targetUserId === userId) {
+		halt(`cannot remove yourself`, 403);
+	}
+
+	const db = drizzle(env.DB_MAIN, { schema });
+
+	const team = await db.query.tTeams.findFirst({ where: and(eq(schema.tTeams.id, teamId), isNull(schema.tTeams.deletedAt)) });
+
+	if (!team) {
+		halt(`team ${teamId} not found`, 404);
+	}
+
+	const membership = await db.query.tMemberships.findFirst({
+		where: and(
+			eq(schema.tMemberships.userId, userId),
+			eq(schema.tMemberships.teamId, teamId),
+			eq(schema.tMemberships.role, schema.membershipRoles.admin)
+		),
+	});
+
+	if (!membership) {
+		halt(`user ${userId} is not an admin of team ${teamId}`, 403);
+	}
+
+	await db.delete(schema.tMemberships).where(and(eq(schema.tMemberships.userId, targetUserId), eq(schema.tMemberships.teamId, teamId)));
+
+	return {};
+};
+
 export const membership_list: ActionHandler = async function (
 	{ env },
 	{
