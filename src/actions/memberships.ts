9ab21acbfae996d1f6d2a023dfd77b4a8b2329e1
GUO YANKE <hi@yankeguo.com>
2024-01-11 16:39:51 +0800

    chore: using DAO for members api

--- src/actions/memberships.ts
+++ src/actions/memberships.ts
@@ -3,6 +3,7 @@ import * as schema from '../../schema-main';
 import { drizzle } from 'drizzle-orm/d1';
 import { sql, and, isNull, eq, inArray } from 'drizzle-orm';
 import { halt } from '../utils';
+import { DAO } from '../dao';
 
 export const membership_add: ActionHandler = async function (
 	{ env },
@@ -26,49 +27,15 @@ export const membership_add: ActionHandler = async function (
 		halt(`invalid role ${role}`, 400);
 	}
 
-	const db = drizzle(env.DB_MAIN, { schema });
+	const dao = new DAO(env);
 
-	const team = await db.query.tTeams.findFirst({ where: and(eq(schema.tTeams.id, teamId), isNull(schema.tTeams.deletedAt)) });
+	await dao.mustTeam(teamId);
 
-	if (!team) {
-		halt(`team ${teamId} not found`, 404);
-	}
+	await dao.mustMembership(teamId, userId, schema.membershipRoles.admin);
 
-	{
-		const membership = await db.query.tMemberships.findFirst({
-			where: and(
-				eq(schema.tMemberships.userId, userId),
-				eq(schema.tMemberships.teamId, teamId),
-				eq(schema.tMemberships.role, schema.membershipRoles.admin)
-			),
-		});
+	await dao.mustUser(targetUserId);
 
-		if (!membership) {
-			halt(`user ${userId} is not an admin of team ${teamId}`, 403);
-		}
-	}
-
-	const targetUser = await db.query.tUsers.findFirst({ where: eq(schema.tUsers.id, targetUserId) });
-
-	if (!targetUser) {
-		halt(`user ${targetUserId} not found`, 404);
-	}
-
-	const membership = await db
-		.insert(schema.tMemberships)
-		.values({
-			id: crypto.randomUUID(),
-			userId: targetUserId,
-			teamId: teamId,
-			role: role,
-			createdAt: new Date(),
-			createdBy: userId,
-		})
-		.onConflictDoUpdate({
-			target: [schema.tMemberships.userId, schema.tMemberships.teamId],
-			set: { role: role },
-		})
-		.returning();
+	const membership = await dao.upsertMembership(teamId, targetUserId, role);
 
 	return { membership };
 };
@@ -89,27 +56,15 @@ export const membership_remove: ActionHandler = async function (
 		halt(`cannot remove yourself`, 403);
 	}
 
+	const dao = new DAO(env);
+
 	const db = drizzle(env.DB_MAIN, { schema });
 
-	const team = await db.query.tTeams.findFirst({ where: and(eq(schema.tTeams.id, teamId), isNull(schema.tTeams.deletedAt)) });
+	await dao.mustTeam(teamId);
 
-	if (!team) {
-		halt(`team ${teamId} not found`, 404);
-	}
+	await dao.mustMembership(teamId, userId, schema.membershipRoles.admin);
 
-	const membership = await db.query.tMemberships.findFirst({
-		where: and(
-			eq(schema.tMemberships.userId, userId),
-			eq(schema.tMemberships.teamId, teamId),
-			eq(schema.tMemberships.role, schema.membershipRoles.admin)
-		),
-	});
-
-	if (!membership) {
-		halt(`user ${userId} is not an admin of team ${teamId}`, 403);
-	}
-
-	await db.delete(schema.tMemberships).where(and(eq(schema.tMemberships.userId, targetUserId), eq(schema.tMemberships.teamId, teamId)));
+	await dao.deleteMembership(teamId, targetUserId);
 
 	return {};
 };
@@ -124,47 +79,13 @@ export const membership_list: ActionHandler = async function (
 		teamId: string;
 	}
 ) {
-	const db = drizzle(env.DB_MAIN, { schema });
+	const dao = new DAO(env);
 
-	const team = await db.query.tTeams.findFirst({ where: and(eq(schema.tTeams.id, teamId), isNull(schema.tTeams.deletedAt)) });
+	await dao.mustTeam(teamId);
 
-	if (!team) {
-		halt(`team ${teamId} not found`, 404);
-	}
+	await dao.mustMembership(teamId, userId, schema.membershipRoles.admin, schema.membershipRoles.member, schema.membershipRoles.viewer);
 
-	const membership = await db.query.tMemberships.findFirst({
-		where: and(
-			eq(schema.tMemberships.userId, userId),
-			eq(schema.tMemberships.teamId, teamId),
-			inArray(schema.tMemberships.role, [schema.membershipRoles.admin, schema.membershipRoles.member, schema.membershipRoles.viewer])
-		),
-	});
+	const memberships = await dao.listMembershipWithUser(teamId);
 
-	if (!membership) {
-		halt(`user ${userId} is not a member or admin of team ${teamId}`, 403);
-	}
-
-	const memberships = await db.query.tMemberships.findMany({ where: eq(schema.tMemberships.teamId, teamId) });
-
-	const membershipUserIds = memberships.map((m) => m.userId);
-
-	const users = await db.query.tUsers.findMany({ where: inArray(schema.tUsers.id, membershipUserIds) });
-
-	return {
-		memberships: memberships.map((m) => {
-			const user = users.find((u) => u.id === m.userId)!;
-			return Object.assign(
-				{},
-				m,
-				user
-					? {
-							userVendor: user.vendor,
-							userVendorUserId: user.vendorUserId,
-							userDisplayName: user.displayName,
-							userCreatedAt: user.createdAt,
-					  }
-					: {}
-			);
-		}),
-	};
+	return { memberships };
 };
--- src/dao.ts
+++ src/dao.ts
@@ -15,13 +15,71 @@ export class DAO {
 		this.db = dirzzleMain(env);
 	}
 
+	async mustUser(userId: string) {
+		const user = await this.db.query.tUsers.findFirst({ where: eq(schema.tUsers.id, userId) });
+		if (!user) {
+			halt(`user ${userId} not found`, 404);
+		}
+		return user;
+	}
+
+	async listMembershipWithUser(teamId: string) {
+		const memberships = await this.db.query.tMemberships.findMany({ where: eq(schema.tMemberships.teamId, teamId) });
+
+		const membershipUserIds = memberships.map((m) => m.userId);
+
+		const users = await this.db.query.tUsers.findMany({ where: inArray(schema.tUsers.id, membershipUserIds) });
+
+		return memberships.map((m) => {
+			const user = users.find((u) => u.id === m.userId)!;
+			return Object.assign(
+				{},
+				m,
+				user
+					? {
+							userVendor: user.vendor,
+							userVendorUserId: user.vendorUserId,
+							userDisplayName: user.displayName,
+							userCreatedAt: user.createdAt,
+					  }
+					: {}
+			);
+		});
+	}
+
+	async deleteMembership(teamId: string, userId: string) {
+		await this.db.delete(schema.tMemberships).where(and(eq(schema.tMemberships.userId, userId), eq(schema.tMemberships.teamId, teamId)));
+	}
+
+	async upsertMembership(teamId: string, userId: string, role: string) {
+		return await this.db
+			.insert(schema.tMemberships)
+			.values({
+				id: crypto.randomUUID(),
+				userId,
+				teamId,
+				role: role,
+				createdAt: new Date(),
+				createdBy: userId,
+			})
+			.onConflictDoUpdate({
+				target: [schema.tMemberships.userId, schema.tMemberships.teamId],
+				set: { role: role },
+			})
+			.returning();
+	}
+
 	async mustMembership(teamId: string, userId: string, ...roles: Array<string>) {
 		if (roles.length === 0) {
 			throw new Error('mustMembership: no roles provided');
 		}
 
 		const membership = await this.db.query.tMemberships.findFirst({
-			where: and(eq(schema.tMemberships.userId, userId), eq(schema.tMemberships.teamId, teamId), inArray(schema.tMemberships.role, roles)),
+			where: and(
+				eq(schema.tMemberships.userId, userId),
+				eq(schema.tMemberships.teamId, teamId),
+				roles.length > 1 ? inArray(schema.tMemberships.role, roles) : eq(schema.tMemberships.role, roles[0])
+			),
 		});
 
 		if (!membership) {
@@ -31,12 +89,8 @@ export class DAO {
 		return membership;
 	}
 
-	async getTeam(teamId: string) {
-		return await this.db.query.tTeams.findFirst({ where: and(eq(schema.tTeams.id, teamId), isNull(schema.tTeams.deletedAt)) });
-	}
-
 	async mustTeam(teamId: string) {
-		const team = this.getTeam(teamId);
+		const team = await this.db.query.tTeams.findFirst({ where: and(eq(schema.tTeams.id, teamId), isNull(schema.tTeams.deletedAt)) });
 		if (!team) {
 			halt(`team ${teamId} not found`, 404);
 		}
