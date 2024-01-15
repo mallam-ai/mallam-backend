9d24ce8acd40b783a19c1ea36bc7cea8fd169b85
GUO YANKE <hi@yankeguo.com>
2024-01-15 14:44:51 +0800

    chore: remove legacy datasets database/queue/api

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,9 +1,7 @@
 import { ActionHandler } from '../types';
-import { datasets_document_upsert, datasets_vectorize_sentences_retrieve, datasets_vectorize_sentences_upsert } from './datasets';
 import { oauth_authorize_user, oauth_create_authorization_uri } from './auth';
 import { user_get } from './users';
 import { team_create, team_list, team_get, team_delete, team_update, team_leave } from './teams';
-import { chat } from './chat';
 import { ping } from './ping';
 import { membership_list, membership_remove, membership_add } from './memberships';
 import {
@@ -19,10 +17,6 @@ import { scheduled } from './scheduled';
 
 const ACTIONS = {
 	ping,
-	datasets_document_upsert,
-	datasets_vectorize_sentences_upsert,
-	datasets_vectorize_sentences_retrieve,
-	chat,
 	oauth_create_authorization_uri,
 	oauth_authorize_user,
 	user_get,
@@ -43,8 +37,6 @@ const ACTIONS = {
 	document_delete,
 	// scheduled
 	scheduled,
-	// queues
-	'queue-mallam-datasets-vectorize-sentences-upsert': datasets_vectorize_sentences_upsert,
 	'queue-mallam-main-document-analyze': document_analyze,
 	'queue-mallam-main-sentence-analyze': sentence_analyze,
 };
